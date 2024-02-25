a71e7a30e4307e811a2b739559ecd123121267eb
Oleksandr <lysakova@yahoo.com>
2024-02-25 18:05:50 +0100

    fix: update cookie options

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,7 +48,7 @@ exports.login = catchAsync(async (req, res, next) => {
   const cookieOptions = {
     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * oneDay),
     httpOnly: true,
-    secure: false,
+    secure: true,
     sameSite: 'None',
   };
 
