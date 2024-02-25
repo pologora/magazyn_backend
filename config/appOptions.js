8985157a021299a32ee9e385ed7b697eec8369ef
Oleksandr <lysakova@yahoo.com>
2024-02-25 18:10:22 +0100

    fix: update corsOptions

--- config/appOptions.js
+++ config/appOptions.js
@@ -2,6 +2,12 @@
 // process.env.CORS_ORIGIN_PROD;
 
 exports.corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:3001'],
+  origin: ['http://localhost:5173',
+    'http://localhost:3001',
+    'http://app.server266411.nazwa.pl',
+    'http://admin.server266411.nazwa.pl',
+    'https://app.server266411.nazwa.pl',
+    'https://admin.server266411.nazwa.pl',
+  ],
   credentials: true, // Allows cookies to be sent from frontend to backend
 };
