a54321a3b579eac1ed98b26b91cf74959721d974
Oleksandr <lysakova@yahoo.com>
2024-02-28 19:46:26 +0100

    FIX: change origin

--- config/appOptions.js
+++ config/appOptions.js
@@ -1,12 +1,9 @@
 exports.corsOptions = {
   origin: [
-    'http://localhost:5173',
-    'http://localhost:3001',
-    'http://localhost:3000',
     'https://www.admin.snti.pl',
-    'https://admin.snti.pl/',
-    'https://www.app.snti.pl/',
-    'https://app.snti.pl/',
+    'https://admin.snti.pl',
+    'https://www.app.snti.pl',
+    'https://app.snti.pl',
   ],
   credentials: true, // Allows cookies to be sent from frontend to backend
 };
