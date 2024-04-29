fa2713a1f85cafa2b732a082d9d97ea54895da49
Oleksandr <lysakova@yahoo.com>
2024-04-29 08:45:52 +0200

    fix: add front-end domains to cors

--- config/appOptions.js
+++ config/appOptions.js
@@ -1,9 +1,4 @@
 exports.corsOptions = {
-  origin: [
-    'https://www.admin.snti.pl',
-    'https://admin.snti.pl',
-    'https://www.app.snti.pl',
-    'https://app.snti.pl',
-  ],
+  origin: ['https://vacations-employees.netlify.app/', 'https://test-magazyn.netlify.app/'],
   credentials: true, // Allows cookies to be sent from frontend to backend
 };
